e4925751f0afc2fdafa89489e5fb72bd8691adf7
Dane David <dane.david@bigbinary.com>
2020-10-27 20:04:52 +0530

    preparation to display tasks list is done

---
+++ app/javascript/components/tasks/List.js
@@ -0,0 +1,14 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+class List extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <p>Task list will be appearing here soon!</p>
+      </React.Fragment>
+    );
+  }
+}
+
+export default List
