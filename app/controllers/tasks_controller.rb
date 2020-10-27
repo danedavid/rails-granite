608b4dc7c40c9ecf7ca8614f014303c96394eb6b
Dane David <dane.david@bigbinary.com>
2020-10-27 20:55:56 +0530

    Implemented create action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,20 @@ class TasksController < ApplicationController
   def new
     @task = Task.new
   end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      render status: :ok, json: { notice: "Task was succesfully created" }
+    else
+      errors = @task.errors.full_messages
+      render status: :unprocessable_entity, json: { errors: errors }
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:description)
+  end
 end
