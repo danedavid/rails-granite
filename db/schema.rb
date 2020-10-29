441e3c830887d631a93edfe76667cb450f296a64
Dane David <dane.david@bigbinary.com>
2020-10-29 21:03:35 +0530

    added has_secure_password to User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_29_103105) do
+ActiveRecord::Schema.define(version: 2020_10_29_151844) do
 
   create_table "tasks", force: :cascade do |t|
     t.text "description", null: false
@@ -23,6 +23,9 @@ ActiveRecord::Schema.define(version: 2020_10_29_103105) do
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "tasks", "users", on_delete: :cascade
