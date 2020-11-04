890a4750a37a1fadd3ad71582d739cb0505f74bf
Dane David <dane.david@bigbinary.com>
2020-11-04 22:33:56 +0530

    Add route for Sidekiq dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username),
+                                                ::Digest::SHA256.hexdigest(ENV['SIDEKIQ_USERNAME'])) &
+      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password),
+                                                  ::Digest::SHA256.hexdigest(ENV['SIDEKIQ_PASSWORD']))
+  end
+  mount Sidekiq::Web, at: '/sidekiq'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :tasks
   resources :users, only: [:new, :create]
