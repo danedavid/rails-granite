4ef2731bff2f972bbeb7729251a7e01960e0bd82
Dane David <dane.david@bigbinary.com>
2020-11-05 09:02:14 +0530

    Redirect root to /tasks

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   end
   mount Sidekiq::Web, at: '/sidekiq'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root :to => redirect('/tasks')
+
   resources :tasks
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create]
